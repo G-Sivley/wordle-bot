97bdcd02fb45a40ca04aeb71d8c2381feba4ee0a
Grant Sivley <grant.sivley@gmail.com>
2022-02-28 20:27:08 -0700

    DataManipulator write_with_removed_words() added

--- app/data_manipulator.py
+++ app/data_manipulator.py
@@ -5,23 +5,39 @@ REMAINING_POSSIBLE_ANSWERS = "word lists/remaining_possible_answers.txt"
 class DataManipulator:
     def __init__(self):
 
-        self.open_and_read_file(STARTER_POSSIBLE_ANSWERS)
+        self.open_and_write_orginal_file(STARTER_POSSIBLE_ANSWERS)
 
     # Init methods
-    def open_and_read_file(self, filename):
+    def open_and_write_orginal_file(self, filename):
         with open(filename, "r") as pa:
-            self.write_remaining_possible_answers(
-                starter_file=pa,
-                filename=REMAINING_POSSIBLE_ANSWERS
-                )
+            with open(REMAINING_POSSIBLE_ANSWERS, "w") as rpa:
+                for line in pa:
+                    rpa.write(line)
 
-    def write_remaining_possible_answers(self, starter_file, filename):
-        with open(filename, "w") as rpa:
-            for line in starter_file:
-                rpa.write(line)
+    def write_with_removed_words(self, letter_to_remove: str):
+        """Removes words from remaining_possible_answers based on letter provided
+
+        Args:
+            letter_to_remove ("character"):
+            The letter that is provided will be
+            removed from all words in the document
+        """
+        with open(REMAINING_POSSIBLE_ANSWERS, "r") as read_file:
+            lines = read_file.readlines()
+            with open(REMAINING_POSSIBLE_ANSWERS, "w") as write_file:
+                remaining_answers = self.remove_words_from_list_with_letter(
+                    lines,
+                    letter_to_remove.lower()
+                    )
+                for line in remaining_answers:
+                    write_file.write(line)
 
     # Use methods
-    def remove_excluded_answers(self, list_of_words: list[str], letter: str):
+    def remove_words_from_list_with_letter(
+        self,
+        list_of_words: list[str],
+        letter: str
+    ):
         return [word for word in list_of_words if letter not in word]
 
     def is_letter_in_word(self, letter: str, word: str):
@@ -30,3 +46,4 @@ class DataManipulator:
 
 if __name__ == "__main__":
     dm = DataManipulator()
+    dm.write_with_removed_words("b")
--- test/test_data_manipulator.py
+++ test/test_data_manipulator.py
@@ -1,35 +1,35 @@
 import unittest
 import pathlib as pl
+from app.data_manipulator import DataManipulator
 import sys
 sys.path.append("../")
 
-from app.data_manipulator import DataManipulator
 
 class TestDataManipulator(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.dm = DataManipulator()
-    
+
     def test_class_init(self):
         self.assertIsInstance(self.dm, DataManipulator)
 
     def test_write_remaining_possible_answers(self):
         path = pl.Path("word lists/remaining_possible_answers.txt")
         self.assertEquals((str(path), path.is_file()), (str(path), True))
-    
+
     def test_is_letter_in_word(self):
         self.assertEqual(self.dm.is_letter_in_word("a", "abott"), True)
         self.assertEqual(self.dm.is_letter_in_word("a", "plane"), True)
         self.assertEqual(self.dm.is_letter_in_word("z", "coins"), False)
         self.assertEqual(self.dm.is_letter_in_word("A", "plane"), True)
-    
+
     def test_remove_excluded_answers(self):
-        self.assertEqual(self.dm.remove_excluded_answers([
+        self.assertEqual(self.dm.remove_words_from_list_with_letter([
             "alpha",
             "wow",
             "brittle",
         ], "a"), ["wow", "brittle"])
-        self.assertEqual(self.dm.remove_excluded_answers([
+        self.assertEqual(self.dm.remove_words_from_list_with_letter([
             "alpha",
             "heart",
             "hobbs",
