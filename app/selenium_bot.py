cb2eea54feb0a2ff1304eaebf86746df80509651
Sazzadul Haque <ssmitrj@gmail.com>
2022-03-03 17:20:14 +0000

    add three functinon in selenium_bot.py: accept_tac, close_instruction and key_press

--- app/selenium_bot.py
+++ app/selenium_bot.py
@@ -1,6 +1,13 @@
 import platform
+from pynput.keyboard import Key, Controller
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+import time
 
 
 class SeleniumBot:
@@ -15,7 +22,7 @@ class SeleniumBot:
             # if macos show app verification problem
             # then go to driver/macOS using terminal
             # then execute: xattr -d com.apple.quarantine chromedriver
-            PATH = "app/driver/MacOS/chromedriver"
+            PATH = "driver/MacOS/chromedriver"
 
         # please test in windows and Linux os
         elif platform.system() == "Windows":
@@ -34,7 +41,46 @@ class SeleniumBot:
         if self.driver:
             self.driver.get(url)
 
+    def accept_tac(self, wait_time: int = 1):
+        btn_id = "pz-gdpr-btn-accept"
+
+        try:
+            get_btn = WebDriverWait(self.driver, wait_time).until(
+                EC.presence_of_element_located((By.ID, btn_id))
+            )
+            get_btn.click()
+
+        except NoSuchElementException as NSE:
+            print(NSE)
+    
+    def close_instruction(self, wait_time: int = 1):
+        # Need to improve
+        self.driver.find_element_by_xpath("//body").click()
+    
+    #buggy function. need to improve
+    def key_press(self, word: str):
+        # pynput working good.
+        # need to improve.
+
+        keyboard = Controller()
+
+        for w in word:
+            keyboard.press(w)
+            keyboard.release(w)
+
+        keyboard.press(Key.enter)
+        time.sleep(3)
+
 
 if __name__ == '__main__':
     sb = SeleniumBot()
     sb.visit_website("https://www.nytimes.com/games/wordle/index.html")
+
+    sb.accept_tac()
+    sb.close_instruction()
+    sb.key_press("pzazz")
+    sb.key_press("jazzy")
+    sb.key_press("buzzy")
+    sb.key_press("fuzzy")
+    sb.key_press("muzzy")
+    sb.key_press("bezzy")
